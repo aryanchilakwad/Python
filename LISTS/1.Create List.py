05019c5896d5dfedd1e215273eac6fd6bbfd0c26
Aryan Chilakwad <aryanchilakwad1@gmail.com>
2026-06-20 00:12:56 +0530

    Add files via upload

---
+++ LISTS/1.Create List.py
@@ -0,0 +1,21 @@
+'''
+Create list in python
+'''
+
+list1 = [1,2,3,4,5]
+print(list1)
+
+list2 = ['a','b','c','d','e']
+print(list2)
+
+list3 = [1,'a',2,'b',3,'c']
+print(list3) #List can contain different data types
+
+'''Operations in list'''
+
+result = list1 + list2
+print(result) #Concatenation of two lists
+
+result2 = list1 * list2
+print(result2) #Repetition of list1 by the number of elements in list2
+
